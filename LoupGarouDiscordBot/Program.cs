737a4772d374f224e9223d554d0ba29661194383
Liador <jean.lousberg@yahoo.fr>
2018-11-27 13:03:48 +0100

    Ajoutez des fichiers projet.

---
+++ LoupGarouDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoupGarouDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
