19f8619e8a3a06384f53ed5b4f2d52df3fce4c3f
Liador <jean.lousberg@yahoo.fr>
2018-11-28 20:07:04 +0100

    First commands added

---
+++ LoupGarouDiscordBot/CommandHandler.cs
@@ -0,0 +1,100 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LoupGarouDiscordBot
+{
+    class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+        private readonly IServiceCollection _map = new ServiceCollection();
+        private IServiceProvider _services = new ServiceCollection().BuildServiceProvider();
+        private CommandService _commands = new CommandService();
+        private string commandPrefix;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+            _service.Log += Log;
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            try
+            {
+                if (s.Channel is IDMChannel)
+                {
+                    if (!s.Author.IsBot)
+                    {
+                        var context = new SocketCommandContext(_client, msg);
+
+                        int argPos = 0;
+                        if (msg.HasStringPrefix(commandPrefix, ref argPos))
+                        {
+                            var result = await _service.ExecuteAsync(context, argPos, _services);
+
+                            if (!result.IsSuccess /*&& result.Error != CommandError.UnknownCommand*/)
+                            {
+                                await context.Channel.SendMessageAsync(result.ErrorReason);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (s.Channel.Id == 516972665261785099 || s.Channel.Id == 340938722830712842)
+                    {
+                        if (!s.Author.IsBot)
+                        {
+                            var context = new SocketCommandContext(_client, msg);
+
+                            int argPos = 0;
+                            if (msg.HasStringPrefix(commandPrefix, ref argPos))
+                            {
+                                var result = await _service.ExecuteAsync(context, argPos, _services);
+
+                                if (!result.IsSuccess /*&& result.Error != CommandError.UnknownCommand*/)
+                                {
+                                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+        public async Task InitCommands(string prefix)
+
+        {
+            commandPrefix = prefix;
+            _services = _map.BuildServiceProvider();
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+
+        }
+    }
+}
---
+++ LoupGarouDiscordBot/Modules/CommandModule.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using System;
+
+namespace LoupGarouDiscordBot.Modules
+{
+    public class CommandModule : ModuleBase<SocketCommandContext>
+    {
+        public CommandModule()
+        {
+        }
+
+        [Command("help", RunMode = RunMode.Async)]
+        public async Task join()
+        {
+            await Context.Channel.SendMessageAsync("Helping...\n but try the \"commands\" command ;) ");
+        }
+
+        [Command("commands", RunMode = RunMode.Async)]
+        public async Task commandList()
+        {
+            await Context.Channel.SendMessageAsync("Les commandes doivent commencer par le préfixe du bot \nHelp : essaye de vous aider \ncommands : affiche la liste des commandes \nping : à tester \nInfos : affiche les commandes pour jouer au loup-garou ainsi que les infos nécessaires au jeu." );
+            //await Context.Channel.SendMessageAsync(Context.Guild.Id.ToString());
+            //await Context.Message.ModifyAsync(Action<Discord.MessageProperties>);
+
+        }
+
+        [Command("ping", RunMode = RunMode.Async)]
+        public async Task ping()
+        {
+            await Context.Channel.SendMessageAsync("pong!");
+        }
+
+        [Command("ping2", RunMode = RunMode.Async)]
+        public async Task truePing()
+        {
+            await Context.Channel.SendMessageAsync(Context.Message.Timestamp - DateTime.Now + " ms");
+        }
+    }
+}
