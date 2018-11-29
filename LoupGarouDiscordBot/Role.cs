ffad52799a8449849572bad15e9203c88ebc728e
Liador <jean.lousberg@yahoo.fr>
2018-11-29 01:20:19 +0100

    Game Creation

---
+++ LoupGarouDiscordBot/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoupGarouDiscordBot
+{
+    class Role
+    {
+        string name;
+        string description;
+        string help;
+        string team;
+        int weight;
+    }
+}
---
+++ LoupGarouDiscordBot/WerewolfGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoupGarouDiscordBot
+{
+    class WerewolfGame
+    {
+        int numberOfPlayers;
+        List<Role> players;
+        public WerewolfGame()
+        {
+            numberOfPlayers = 0;
+        }
+
+        public WerewolfGame(int number)
+        {
+            numberOfPlayers = number;
+        }
+
+        public void startGame()
+        {
+
+        }
+
+        public void addNewPlayer()
+        {
+            numberOfPlayers++;
+        }
+
+        private void createComp()
+        {
+            int werewolfWeight = numberOfPlayers / 6 + 1;
+            int numberOfRolesAdded=0;
+
+            //players.Add();
+        }
+    }
+}
